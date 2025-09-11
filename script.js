e70f5d31db28696a1c587c0f0d811527aac701d1
mirellasantos <ferreira.santos.mirella@escola.pr.gov.br>
2025-09-11 14:31:55 +0000

    salvar

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.addEvenlistener('DOMContentLoaded', function () {
     // Utilitário para formatar moeda (R$)
     function moedaBR(valor ) {
         return new IntL.NumberFormat('pt-BR',{ style: 'currency',  currency :
+            'BRL' }).format(valor);
+        }
+        // normaliza entrada (troca vírugula por ponto e converte para número)
+        function toNumber(val) {
+            if (typeof val === 'number') return val;
+            if (!val && val ! == 0) return NaN;
